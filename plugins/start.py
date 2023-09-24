d7ee464f5eb1bc923b2edae49b8fe48d061fb05f
Zkhan12345 <143610535+Zkhan12345@users.noreply.github.com>
2023-09-24 20:33:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -10,7 +10,7 @@ from helper.database import  insert ,find_one,used_limit,usertype,uploadlimit,ad
 from pyrogram.file_id import FileId
 from helper.database import daily as daily_
 from helper.date import add_date ,check_expi
-CHANNEL = os.environ.get('CHANNEL',"")
+CHANNEL = os.environ.get('CHANNEL',"WebXBots")
 import datetime
 from datetime import date as date_
 STRING = os.environ.get("STRING","")
